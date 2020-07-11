5276e346bb62fef802b021f39b94962ae29db635
Matthew Gould <matthew.gould@influentialsoftware.com>
2020-07-11 10:16:31 +0100

    First commit. Project initialisation.

---
+++ src/main/java/com/dwp/users/dwplondonusers/DwpLondonUsersApplication.java
@@ -0,0 +1,13 @@
+package com.dwp.users.dwplondonusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DwpLondonUsersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DwpLondonUsersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dwp/users/dwplondonusers/DwpLondonUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dwp.users.dwplondonusers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DwpLondonUsersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
