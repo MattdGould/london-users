c60d5a8a6d789d6423292a5c10d9a5db15890f32
Matthew Gould <matthew.gould@influentialsoftware.com>
2020-07-11 10:45:59 +0100

    Basic swagger docs addded

--- src/main/java/com/dwp/users/dwplondonusers/controller/HelloWorldController.java
+++ src/main/java/com/dwp/users/dwplondonusers/controller/HelloWorldController.java
@@ -1,13 +1,13 @@
 package com.dwp.users.dwplondonusers.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
-    @RequestMapping("/hello")
+    @GetMapping("/hello")
     public String sayHello(@RequestParam(value="name") String name) {
         return "Hello " + name;
     }
