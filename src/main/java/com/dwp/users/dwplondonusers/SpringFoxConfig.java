a8547beaf63218b50685ec8a6795ca18c2e6a9da
Matthew Gould <matthew.gould@influentialsoftware.com>
2020-07-12 11:54:11 +0100

    Updated Swagger documentation

--- src/main/java/com/dwp/users/dwplondonusers/SpringFoxConfig.java
+++ src/main/java/com/dwp/users/dwplondonusers/SpringFoxConfig.java
@@ -1,5 +1,6 @@
 package com.dwp.users.dwplondonusers;
 
+import com.google.common.base.Predicates;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
@@ -17,7 +18,7 @@ public class SpringFoxConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
+                .paths(Predicates.not(PathSelectors.regex("/error.*")))
                 .build();
     }
 
---
+++ src/main/java/com/dwp/users/dwplondonusers/controller/UsersLondonController.java
@@ -0,0 +1,54 @@
+package com.dwp.users.dwplondonusers.controller;
+
+import com.dwp.users.dwplondonusers.model.DwpUserModel;
+import com.dwp.users.dwplondonusers.service.UserToLondonDistanceService;
+import com.dwp.users.dwplondonusers.service.UsersService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@Api(tags = "London users Queries", value = "LondonUserQueries")
+public class UsersLondonController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(UsersLondonController.class);
+
+    private final UserToLondonDistanceService userToLondonDistanceService;
+    private final UsersService usersService;
+
+    public UsersLondonController(UserToLondonDistanceService userToLondonDistanceService, UsersService usersService) {
+        this.userToLondonDistanceService = userToLondonDistanceService;
+        this.usersService = usersService;
+    }
+
+    @ApiOperation(value = "Finds all users within 50 miles of London")
+    @GetMapping(value = "/users/London/catchment", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<DwpUserModel> getUsersNearLondon() {
+        LOGGER.info("entering /users/NearLondon");
+        List<DwpUserModel> allUsers =  usersService.findAll();
+        LOGGER.info("All users Size: " + allUsers.size());
+        List<DwpUserModel> usersNearLondon = userToLondonDistanceService
+                .findUsersWithinMilesOfLondon(allUsers, 50);
+        LOGGER.info("Users Near London Size: " + usersNearLondon.size());
+        return usersNearLondon;
+    }
+
+    @ApiOperation(value = "Finds all users within requested miles of London")
+    @GetMapping(value = "/users/London", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<DwpUserModel> getUsersNearLondon(@RequestParam(value="distanceFrom") double distance) {
+        LOGGER.info("entering /users/London");
+        List<DwpUserModel> allUsers =  usersService.findAll();
+        LOGGER.info("All users Size: " + allUsers.size());
+        List<DwpUserModel> usersNearLondon = userToLondonDistanceService
+                .findUsersWithinMilesOfLondon(allUsers, distance);
+        LOGGER.info("Users within " + distance + " miles of London Size: " + usersNearLondon.size());
+        return usersNearLondon;
+    }
+}
