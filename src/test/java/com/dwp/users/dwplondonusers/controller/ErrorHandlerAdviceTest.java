ebae847ace6be10ed3c489e5fb4dc0609a5e5f86
Matthew Gould <matthew.gould@influentialsoftware.com>
2020-07-12 18:25:08 +0100

    Optimised imports

--- src/test/java/com/dwp/users/dwplondonusers/controller/ErrorHandlerAdviceTest.java
+++ src/test/java/com/dwp/users/dwplondonusers/controller/ErrorHandlerAdviceTest.java
@@ -6,16 +6,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.client.HttpClientErrorException;
 
-import static org.hamcrest.Matchers.containsString;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @WebMvcTest
 class ErrorHandlerAdviceTest {
